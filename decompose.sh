f55aa251bcce9e59232352badaf1b123dce3f956
Jan De Rudder <39810873+janderudder@users.noreply.github.com>
2023-01-06 20:38:42 +0100

    feat - add decompose function

---
+++ decompose.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#	decompose <function>
+#
+#	For a given input and single character separator, output each field on
+#	a seperate line.
+#
+#	Requires Bash 4.4 or greater.
+#
+#	Example
+#
+#		$ decompose "$PATH" :
+#
+decompose()
+{
+	local -r Input="$1"
+	local -r Separator="${2-$'\n'}"
+
+	local -a Array=()
+
+	mapfile -d"$Separator" -t Array <<< "$Input"
+
+	# output of the script
+	for Component in "${Array[@]}"
+	do
+		printf '%s\n' "$Component"
+	done
+}
---
+++ decompose_posix.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+#	decompose <function>
+#
+#	For a given input and single character separator, output each field on
+#	a seperate line.
+#
+#	POSIX compliant.
+#
+#	Example
+#
+#		$ decompose "$PATH" :
+#
+decompose()
+{
+	Input="$1"
+	Sep="${2-\n}"
+	i=1
+
+	Component="$(echo "$Input" | cut -d"$Sep" -f$i)"
+
+	while test "$Component"
+	do
+		echo "$Component"
+		i=$((i+1))
+		Component="$(echo "$Input" | cut -d"$Sep" -f$i)"
+	done
+}
